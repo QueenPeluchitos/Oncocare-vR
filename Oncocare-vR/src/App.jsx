f0ecefca7712bec41d63fd20fb5ea8ce8228989a
ArgelGael <130200555+ArgelGael@users.noreply.github.com>
2024-11-23 16:15:56 -0600

    First Section and Comp try

--- Oncocare-vR/src/App.jsx
+++ Oncocare-vR/src/App.jsx
@@ -1,8 +1,11 @@
 import Tratamientos from "./features/Tratamientos/Tratamientos";
-
+import "./App.css"
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="bg-blue-500 text-white text-center p-4 rounded">
+        Si esto es azul con texto blanco, Tailwind está funcionando.
+      </div>
       <Tratamientos />  
     </div>
   );
--- Oncocare-vR/src/features/Tratamientos/Tratamientos.jsx
+++ Oncocare-vR/src/features/Tratamientos/Tratamientos.jsx
@@ -1,55 +1,46 @@
+import React from 'react';
+import Card from './components/Card';
+
 const Tratamientos = () => {
-  const treatments = [
+  const blogs = [
     {
-      title: "Teeth Whitening",
-      description: "Let us show you how our experience.",
-      image: "/images/teeth-whitening.svg",
-      link: "#",
+      category: "Business",
+      date: "29 Nov, 2019",
+      title: "Card 1",
+      description: "This card has supporting text below as a natural lead-in to additional content.",
+      author: "Abby Sims",
     },
     {
-      title: "Oral Surgery",
-      description: "Let us show you how our experience.",
-      image: "/images/oral-surgery.svg",
-      link: "#",
-    },
-    {
-      title: "Painless Dentistry",
-      description: "Let us show you how our experience.",
-      image: "/images/painless-dentistry.svg",
-      link: "#",
-    },
-    {
-      title: "Periodontics",
-      description: "Let us show you how our experience.",
-      image: "/images/periodontics.svg",
-      link: "#",
+      category: "Business",
+      date: "29 Nov, 2019",
+      title: "Card 2",
+      description: "This card has supporting text below as a natural lead-in to additional content.",
+      author: "Abby Sims",
     },
   ];
 
   return (
-    <section className="relative px-4 py-16 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 lg:py-32">
-      <div className="md:flex md:flex-wrap mt-24 text-center md:-mx-4">
-        {treatments.map((treatment, index) => (
-          <div
+    <section className="relative bg-white px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-32">
+      <div>
+        <h2 className="text-3xl leading-tight font-bold">Health Blog</h2>
+        <p className="text-gray-600 mt-2 md:max-w-lg">
+          Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+        </p>
+        <a href="#" title="" className="inline-block text-teal-500 font-semibold mt-6">
+          View All Posts
+        </a>
+      </div>
+
+      <div className="md:flex mt-12 md:-mx-4">
+        {blogs.map((blog, index) => (
+          <Card
             key={index}
-            className="md:w-1/2 md:px-4 lg:w-1/4 mt-4 lg:mt-0"
-          >
-            <div className="bg-white rounded-lg border border-gray-300 p-8">
-              <img
-                src={treatment.image}
-                alt={treatment.title}
-                className="h-20 mx-auto"
-              />
-              <h4 className="text-xl font-bold mt-4">{treatment.title}</h4>
-              <p className="mt-1">{treatment.description}</p>
-              <a
-                href={treatment.link}
-                className="block mt-4 text-teal-500 hover:underline"
-              >
-                Read More
-              </a>
-            </div>
-          </div>
+            category={blog.category}
+            date={blog.date}
+            title={blog.title}
+            description={blog.description}
+            author={blog.author}
+          />
         ))}
       </div>
     </section>
